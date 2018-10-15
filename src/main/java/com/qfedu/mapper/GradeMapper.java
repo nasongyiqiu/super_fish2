2a456b97e1e3b8e12009066816c85dd943c7cda0
renr <123444@aliyun.com>
2018-10-15 23:11:47 +0800

    添加了一个三级列表

--- src/main/java/com/qfedu/mapper/GradeMapper.java
+++ src/main/java/com/qfedu/mapper/GradeMapper.java
@@ -2,16 +2,14 @@ package com.qfedu.mapper;
 
 import com.qfedu.domain.Grade;
 
+import java.util.List;
+
 public interface GradeMapper {
-    int deleteByPrimaryKey(Integer id);
+//返回一级权限的方法
+      List<Grade> grade();
+    //返回二级权限的方法
+    List<Grade> twograde(int id);
+    //返回三级权限的方法
+    List<Grade> threegrade(int id);
 
-    int insert(Grade record);
-
-    int insertSelective(Grade record);
-
-    Grade selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(Grade record);
-
-    int updateByPrimaryKey(Grade record);
 }
